3d25ee747dd5fac6feb3ba46201286bc0ccf5415
behe95 <abid.ahsanahmed@gmail.com>
2024-02-25 13:12:54 -0500

    project initialization

---
+++ src/main/java/com/example/cntrctmgmt/CntrctmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.cntrctmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CntrctmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CntrctmgmtApplication.class, args);
+	}
+
+}
