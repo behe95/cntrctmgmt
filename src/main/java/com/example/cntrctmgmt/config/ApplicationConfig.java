ba55eb33d3095d4f035f8ffa6e2eabad93b63688
behe95 <abid.ahsanahmed@gmail.com>
2024-02-28 00:47:00 -0500

    designed sql db table for contract

--- src/main/java/com/example/cntrctmgmt/config/ApplicationConfig.java
+++ src/main/java/com/example/cntrctmgmt/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.example.cntrctmgmt.config;
 
 import com.example.cntrctmgmt.constant.db.DBCategoryConst;
+import com.example.cntrctmgmt.constant.db.DBContractConst;
 import com.example.cntrctmgmt.constant.db.DBSubCategoryConst;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -34,5 +35,11 @@ public class ApplicationConfig implements CommandLineRunner {
         this.jdbcTemplate.execute(DBSubCategoryConst._DB_CREATE_SUBCATEGORY_TABLE_IF_NOT_EXISTS);
         this.jdbcTemplate.execute(DBSubCategoryConst._DB_CREATE_SUBCATEGORY_TABLE_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
         this.jdbcTemplate.execute(DBSubCategoryConst._DB_CREATE_SUBCATEGORY_TABLE_MODIFIED_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
+
+        // contract table
+        this.jdbcTemplate.execute(DBContractConst._DB_CREATE_CONTRACT_TABLE_IF_NOT_EXISTS);
+        this.jdbcTemplate.execute(DBContractConst._DB_CREATE_CONTRACT_TABLE_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
+        this.jdbcTemplate.execute(DBContractConst._DB_CREATE_CONTRACT_TABLE_MODIFIED_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
+
     }
 }
