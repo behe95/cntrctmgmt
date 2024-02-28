9ca9caa63c4a64efa60e87bfe3e8b06b9803ca2f
behe95 <abid.ahsanahmed@gmail.com>
2024-02-27 19:12:22 -0500

    database design for the sub category table

--- src/main/java/com/example/cntrctmgmt/config/ApplicationConfig.java
+++ src/main/java/com/example/cntrctmgmt/config/ApplicationConfig.java
@@ -1,12 +1,13 @@
 package com.example.cntrctmgmt.config;
 
 import com.example.cntrctmgmt.constant.db.DBCategoryConst;
+import com.example.cntrctmgmt.constant.db.DBSubCategoryConst;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-//@Configuration
+@Configuration
 public class ApplicationConfig implements CommandLineRunner {
     private final JdbcTemplate jdbcTemplate;
 
@@ -24,8 +25,14 @@ public class ApplicationConfig implements CommandLineRunner {
 //        this.jdbcTemplate.execute("PRAGMA foreign_keys=ON;");
 //        this.jdbcTemplate.execute("PRAGMA recursive_triggers = OFF;");
         // create category table;
+        // category table
         this.jdbcTemplate.execute(DBCategoryConst._DB_CREATE_CATEGORY_TABLE_IF_NOT_EXISTS);
         this.jdbcTemplate.execute(DBCategoryConst._DB_CREATE_CATEGORY_TABLE_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
         this.jdbcTemplate.execute(DBCategoryConst._DB_CREATE_CATEGORY_TABLE_MODIFIED_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
+
+        // subcategory table
+        this.jdbcTemplate.execute(DBSubCategoryConst._DB_CREATE_SUBCATEGORY_TABLE_IF_NOT_EXISTS);
+        this.jdbcTemplate.execute(DBSubCategoryConst._DB_CREATE_SUBCATEGORY_TABLE_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
+        this.jdbcTemplate.execute(DBSubCategoryConst._DB_CREATE_SUBCATEGORY_TABLE_MODIFIED_DATE_UPDATE_TRIGGER_IF_NOT_EXISTS);
     }
 }
