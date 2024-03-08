a236b54ff0588ec0536836a45a5307cdd2589ba9
behe95 <abid.ahsanahmed@gmail.com>
2024-03-07 22:45:13 -0500

    refactored code - fixed minor bug

--- src/main/java/com/example/cntrctmgmt/constant/responsemessage/EndUserResponseMessage.java
+++ src/main/java/com/example/cntrctmgmt/constant/responsemessage/EndUserResponseMessage.java
@@ -2,20 +2,32 @@ package com.example.cntrctmgmt.constant.responsemessage;
 
 public enum EndUserResponseMessage {
     CATEGORY_SAVED("Category saved successfully!"),
+    CATEGORY_SAVED_ERROR("Something went wrong! Unable to sav category!"),
     CATEGORY_UPDATED("Category updated successfully!"),
+    CATEGORY_UPDATED_ERROR("Something went wrong! Unable to update category!"),
     CATEGORY_DELETED("Category deleted successfully!"),
+    CATEGORY_DELETED_ERROR("Something went wrong! Unable to delete category!"),
 
     SUBCATEGORY_SAVED("Sub-category saved successfully!"),
+    SUBCATEGORY_SAVED_ERROR("Something went wrong! Something went wrong! Unable to save sub-category!"),
     SUBCATEGORY_UPDATED("Sub-category updated successfully!"),
+    SUBCATEGORY_UPDATED_ERROR("Something went wrong! Unable to update sub-category!"),
     SUBCATEGORY_DELETED("Sub-category deleted successfully!"),
+    SUBCATEGORY_DELETED_ERROR("Something went wrong! Unable to delete sub-category!"),
 
     CONTRACT_SAVED("Contract saved successfully!"),
+    CONTRACT_SAVED_ERROR("Something went wrong! Unable to save contract!"),
     CONTRACT_UPDATED("Contract updated successfully!"),
+    CONTRACT_UPDATED_ERROR("Something went wrong! Unable to update contract!"),
     CONTRACT_DELETED("Contract deleted successfully!"),
+    CONTRACT_DELETED_ERROR("Something went wrong! Unable to delete contract!"),
 
     SUBCONTRACT_SAVED("Sub-contract saved successfully!"),
+    SUBCONTRACT_SAVED_ERROR("Something went wrong! Unable to save sub-contract!"),
     SUBCONTRACT_UPDATED("Sub-contract updated successfully!"),
-    SUBCONTRACT_DELETED("Sub-contract deleted successfully!");
+    SUBCONTRACT_UPDATED_ERROR("Something went wrong! Unable to update sub-contract!"),
+    SUBCONTRACT_DELETED("Sub-contract deleted successfully!"),
+    SUBCONTRACT_DELETED_ERROR("Something went wrong! Unable to delete sub-contract!");
 
     private final String message;
     EndUserResponseMessage(String message) {
--- src/main/java/com/example/cntrctmgmt/controllers/SettingsCategoryViewController.java
+++ src/main/java/com/example/cntrctmgmt/controllers/SettingsCategoryViewController.java
@@ -1,6 +1,7 @@
 package com.example.cntrctmgmt.controllers;
 
 
+import com.example.cntrctmgmt.constant.responsemessage.EndUserResponseMessage;
 import com.example.cntrctmgmt.entities.Category;
 import com.example.cntrctmgmt.entities.SubCategory;
 import com.example.cntrctmgmt.exceptions.DuplicateEntityException;
@@ -12,30 +13,27 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.*;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Controller
 public class SettingsCategoryViewController {
 
+
     private final CategoryService categoryService;
     private final SubCategoryService subCategoryService;
 
-    private ObservableList<Category> categories;
-    private ObservableList<SubCategory> subCategories;
+    private ObservableList<Category> categoryObservableList;
+    private ObservableList<SubCategory> subCategoryObservableList;
 
     @FXML
     private ListView<Category> listViewCategory;
@@ -46,24 +44,22 @@ public class SettingsCategoryViewController {
     @FXML
     private ListView<SubCategory> listViewAssignedSubCategory;
 
-    @PersistenceContext
-    private EntityManager entityManager;
 
     @Autowired
     public SettingsCategoryViewController(CategoryService categoryService, SubCategoryService subCategoryService) {
         this.categoryService = categoryService;
         this.subCategoryService = subCategoryService;
-        categories = FXCollections.observableArrayList(new ArrayList<>());
-        subCategories = FXCollections.observableArrayList(new ArrayList<>());
+        categoryObservableList = FXCollections.observableArrayList(new ArrayList<>());
+        subCategoryObservableList = FXCollections.observableArrayList(new ArrayList<>());
     }
 
 
     @FXML
     private void initialize() {
         // Get all the categories
-        categories.setAll(categoryService.getAllCategories());
+        categoryObservableList.setAll(categoryService.getAllCategories());
         // Get all the subcategories
-        subCategories.setAll(subCategoryService.getAllSubCategories());
+        subCategoryObservableList.setAll(subCategoryService.getAllSubCategories());
 
         // populate list views
         initListViewCategory();
@@ -72,9 +68,20 @@ public class SettingsCategoryViewController {
     }
 
 
-
+    /**
+     * All the available categories will be presented.
+     * Method populates the ListView with all the available categories
+     * from queried from the database.
+     * The ListView is editable.
+     * Each ListCell is represented with TextFieldListCell to provide editing
+     * option to the end user.
+     * On selection of any category from the list, the ListView for the
+     * assigned sub-categories and available sub-categories to assign to this category will be populated.
+     * User can right-click on any of the ListCell that will pop-up a context menu for
+     * further interaction.
+     */
     private void initListViewCategory() {
-        listViewCategory.setItems(categories);
+        listViewCategory.setItems(categoryObservableList);
         listViewCategory.setEditable(true);
 
 
@@ -89,7 +96,7 @@ public class SettingsCategoryViewController {
                         setText(category.getTitle());
                     }
 
-
+                    // convert object to make it compatible for the TextFieldListCell
                     @Override
                     public void updateItem(Category category, boolean empty) {
                         super.updateItem(category, empty);
@@ -98,6 +105,7 @@ public class SettingsCategoryViewController {
                             public String toString(Category category) {
                                 return category.getTitle();
                             }
+
                             @Override
                             public Category fromString(String s) {
                                 getItem().setTitle(s);
@@ -105,89 +113,27 @@ public class SettingsCategoryViewController {
                             }
                         });
 
-                        
 
-
-
-                        // on Category selection
+                        // show available sub-categories and assigned categories
                         this.setOnMouseClicked(mouseEvent -> {
                             if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
                                 if (Objects.nonNull(category) && !empty) {
                                     listViewAssignedSubCategory.setItems(category.getSubCategoryList());
                                     listViewAvailableSubCategory
-                                            .setItems(FXCollections.observableArrayList(
-                                                    subCategories.filtered(
-                                                            subCategory -> category.getSubCategoryList().stream()
-                                                                    .noneMatch(subCategory1 -> subCategory1.getId() == subCategory.getId())
-                                                    )));
+                                    .setItems(FXCollections.observableArrayList(
+                                            subCategoryObservableList
+                                                    .filtered(
+                                                    subCategory -> category.getSubCategoryList().stream()
+                                                            .noneMatch(subCategory1 -> subCategory1.getId() == subCategory.getId())
+                                                    )
+                                            )
+                                    );
                                 }
                             }
 
+                            // show context menu for interacting with the selected list-cell
                             if (mouseEvent.getButton().equals(MouseButton.SECONDARY)) {
-                                ContextMenu contextMenu = new ContextMenu();
-                                MenuItem saveMenutItem = new MenuItem("Save");
-                                MenuItem addMenutItem = new MenuItem("Add New");
-                                MenuItem editMenuItem = new MenuItem("Edit");
-                                MenuItem deleteMenuItem = new MenuItem("Delete");
-                                MenuItem cancelMenuItem = new MenuItem("Cancel");
-
-                                saveMenutItem.setOnAction(actionEvent -> {
-                                    try {
-                                        Category category1 = listViewProperty().get().getSelectionModel().getSelectedItem();
-                                        String message = "Category updated!";
-                                        if (category1.getId() <= 0) {
-                                            categoryService.addCategory(category1);
-                                            message = "Category saved!";
-                                        } else {
-                                            categoryService.updateCategory(category1);
-                                        }
-
-                                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                                        alert.setContentText(message);
-                                        alert.showAndWait();
-                                    } catch (DuplicateEntityException e) {
-                                        Alert alert = new Alert(Alert.AlertType.ERROR);
-                                        alert.setContentText(e.getMessage());
-                                        alert.showAndWait();
-                                        this.startEdit();
-                                    }
-                                });
-
-                                deleteMenuItem.setOnAction(actionEvent -> {
-                                    try {
-                                        Category category1 = listViewProperty().get().getSelectionModel().getSelectedItem();
-                                        String message = "Category deleted!";
-
-                                        categoryService.deleteCategory(category);
-
-                                        listViewCategory.itemsProperty().get().remove(category1);
-                                        listViewCategory.getSelectionModel().clearSelection();
-
-                                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                                        alert.setContentText(message);
-                                        alert.showAndWait();
-
-                                    } catch (Exception ex) {
-                                        Alert alert = new Alert(Alert.AlertType.ERROR);
-                                        alert.setContentText("Something went wrong! Unable to delete at this time.");
-                                        alert.showAndWait();
-                                    }
-                                });
-
-                                /**
-                                 * TODO
-                                 */
-                                addMenutItem.setOnAction(actionEvent -> {
-                                    Category newCategory = new Category();
-                                    listViewCategory.itemsProperty().get().add(newCategory);
-                                    listViewCategory.getSelectionModel().select(newCategory);
-                                    listViewCategory.edit(listViewCategory.itemsProperty().get().size()-1);
-                                });
-
-                                editMenuItem.setOnAction(actionEvent -> startEdit());
-
-                                contextMenu.getItems().addAll(saveMenutItem, addMenutItem, editMenuItem, deleteMenuItem, cancelMenuItem);
-                                this.setContextMenu(contextMenu);
+                                this.setContextMenu(getCustomContextMenu(this));
                             }
                         });
                     }
@@ -196,9 +142,15 @@ public class SettingsCategoryViewController {
         });
     }
 
+    /**
+     * All the available sub-categories will be presented that can be assigned to a selected category.
+     * Method populates the ListView with all the available sub-categories
+     * except for the assigned sub-categories.
+     * End user can double-click on any of the sub-categories which will move
+     * the sub-category to the assigned sub-category.
+     */
     private void initListViewAvailableSubCategory() {
         // Show SubCategory title in listViewAvailableSubCategory
-
         listViewAvailableSubCategory.setCellFactory(new Callback<ListView<SubCategory>, ListCell<SubCategory>>() {
             @Override
             public ListCell<SubCategory> call(ListView<SubCategory> subCategoryListView) {
@@ -212,12 +164,17 @@ public class SettingsCategoryViewController {
                             setText(null);
                         }
 
+                        // double click to assign the sub-category
                         this.setOnMouseClicked(new EventHandler<MouseEvent>() {
                             @Override
                             public void handle(MouseEvent mouseEvent) {
                                 if (mouseEvent.getClickCount() == 2 && Objects.nonNull(subCategory)) {
+                                    // remove the sub-category from the available sub-category list
                                     listViewAvailableSubCategory.getItems().remove(subCategory);
+                                    // assign the sub-category to the selected category
                                     listViewCategory.getSelectionModel().getSelectedItem().getSubCategoryList().add(subCategory);
+                                    // assign the selected category to the sub-category that has been assigned
+                                    // to make association in the persistence context
                                     subCategory.getCategoryList().add(listViewCategory.getSelectionModel().getSelectedItem());
                                 }
                             }
@@ -228,6 +185,13 @@ public class SettingsCategoryViewController {
         });
     }
 
+    /**
+     * All the assigned sub-categories will be presented.
+     * Method populates the ListView with all the assigned sub-categories
+     * except for the un-assigned sub-categories.
+     * End user can double-click on any of the sub-categories which will move
+     * the sub-category to the ListView that contains un-assigned or available sub-categories.
+     */
     private void initListViewAssignedSubCategory() {
         // Show SubCategory title in listviewAssignedSubCategory
         listViewAssignedSubCategory.setCellFactory(new Callback<ListView<SubCategory>, ListCell<SubCategory>>() {
@@ -239,17 +203,22 @@ public class SettingsCategoryViewController {
                         super.updateItem(subCategory, empty);
                         if (Objects.nonNull(subCategory) && !empty) {
                             setText(subCategory.getTitle());
-                        }else {
+                        } else {
                             setText(null);
                         }
 
+                        // double click to un-assign the sub-category
                         this.setOnMouseClicked(new EventHandler<MouseEvent>() {
                             @Override
                             public void handle(MouseEvent mouseEvent) {
                                 if (mouseEvent.getClickCount() == 2 && Objects.nonNull(subCategory)) {
+                                    // add the sub-category to the list of available sub-categories
                                     listViewAvailableSubCategory.getItems().add(subCategory);
+                                    // remove the sub-category from the assigned sub-category list
                                     listViewCategory.getSelectionModel().getSelectedItem().getSubCategoryList().remove(subCategory);
-                                    subCategory.getCategoryList().add(listViewCategory.getSelectionModel().getSelectedItem());
+                                    // remove the selected category from the sub-category that has been un-assigned
+                                    // to remove the association in the persistence context
+                                    subCategory.getCategoryList().remove(listViewCategory.getSelectionModel().getSelectedItem());
                                 }
                             }
                         });
@@ -258,6 +227,86 @@ public class SettingsCategoryViewController {
             }
         });
     }
+
+    /**
+     * Creates a Context menu for user interaction with a selected ListCell
+     * @param textFieldListCell ListCell that will be interacted
+     * @return  Context Menu
+     */
+    private ContextMenu getCustomContextMenu(TextFieldListCell<Category> textFieldListCell) {
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem saveMenutItem = new MenuItem("Save");
+        MenuItem addMenutItem = new MenuItem("Add New");
+        MenuItem editMenuItem = new MenuItem("Edit");
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        MenuItem cancelMenuItem = new MenuItem("Cancel");
+
+        // save or update category
+        // throws exception if duplicate category
+        // is being saved
+        saveMenutItem.setOnAction(actionEvent -> {
+            try {
+                Category category1 = textFieldListCell.listViewProperty()
+                        .get().getSelectionModel().getSelectedItem();
+                if (category1.getId() <= 0) {
+                    categoryService.addCategory(category1);
+                } else {
+                    categoryService.updateCategory(category1);
+                }
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText(EndUserResponseMessage.CATEGORY_SAVED.getMessage());
+                alert.showAndWait();
+            } catch (DuplicateEntityException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText(EndUserResponseMessage.CATEGORY_SAVED_ERROR + " " + e.getMessage());
+                alert.showAndWait();
+                textFieldListCell.startEdit();
+            }
+        });
+
+        // delete category
+        deleteMenuItem.setOnAction(actionEvent -> {
+            try {
+                Category category1 = textFieldListCell.listViewProperty().get().getSelectionModel().getSelectedItem();
+
+                Category category = textFieldListCell.getItem();
+
+                categoryService.deleteCategory(category);
+
+                listViewCategory.itemsProperty().get().remove(category1);
+                listViewCategory.getSelectionModel().clearSelection();
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText(EndUserResponseMessage.CATEGORY_DELETED.getMessage());
+                alert.showAndWait();
+
+            } catch (Exception ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText(EndUserResponseMessage.CATEGORY_DELETED_ERROR.getMessage());
+                alert.showAndWait();
+            }
+        });
+
+        // add a new category item at the end of the ListView
+        // automatically focus and start editing once added
+        addMenutItem.setOnAction(actionEvent -> {
+            Category newCategory = new Category();
+            listViewCategory.itemsProperty().get().add(newCategory);
+            listViewCategory.getSelectionModel().select(newCategory);
+            listViewCategory.edit(listViewCategory.itemsProperty().get().size() - 1);
+        });
+
+        editMenuItem.setOnAction(actionEvent -> textFieldListCell.startEdit());
+
+
+        contextMenu.getItems().addAll(saveMenutItem, addMenutItem, editMenuItem, deleteMenuItem, cancelMenuItem);
+
+        return contextMenu;
+
+    }
+
+
 
 
 }
